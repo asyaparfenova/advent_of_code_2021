f62d05ff4c3ee37b1b6c9f71c87dbf1170bd52bb
anna-parfenova <anna.parfenova@deliveryhero.com>
2021-12-02 19:36:31 +0100

    day1_postformatting

--- day1.py
+++ day1.py
@@ -1,30 +1,20 @@
-"""This programm calculates the answer
-for day 1 challenges of Advent of Code 2021"""
+"""This program calculates answers
+for day-1 challenges of Advent-of-Code-2021"""
 
 import numpy as np
 
-
-def get_increases(numbers):
-    """Count the number of times a depth measurement increases;
-    numbers: list"""
+def get_increases(measurements, n):
+    """Count the number of times a depth measurement increases for sum of n measurements;
+    n(int): sum of how many measurements are we taking for comparison
+    measurements (list of int): measurements taken
+    returnes (int): count of increases"""
     incr = 0
-    for i in range(1, len(numbers)):
-        if numbers[i] > numbers[i - 1]:
+    for i in range(n, len(numbers)):
+        if numbers[i] > numbers[i - n]:
             incr += 1
     return incr
-
-
-def get_triple_increases(numbers):
-    """Count the number of times the sum of measurements in this three-measurement-sliding window increases;
-    numbers: list"""
-    incr = 0
-    for i in range(3, len(numbers)):
-        if numbers[i] > numbers[i - 3]:
-            incr += 1
-    return incr
-
 
 if __name__ == "__main__":
     mylist = np.loadtxt("day1_task.txt", dtype=int)
-    print(f"The answer for the 1st task is: {get_increases(mylist)}")
-    print(f"The answer for the 2nd task is: {get_triple_increases(mylist)}")
+    print(f"The answer for the 1st task is: {get_increases(mylist, 1)}")
+    print(f"The answer for the 2nd task is: {get_increases(mylist, 3)}")
