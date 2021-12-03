d79d68bdcbab35cd395f9ad003bd97468779ad77
anna-parfenova <anna.parfenova@deliveryhero.com>
2021-12-03 15:13:33 +0100

    third day solution edited not optimised

--- day3.py
+++ day3.py
@@ -1,65 +1,93 @@
 """This programm calculates the answer
-for day 1 challenges of Advent of Code 2021"""
+for day 3 challenges of Advent of Code 2021"""
 
 import re
 
+
 def get_input_numbers(input_data):
-    '''extract input numbers from input data'''
-    num_re = r'([0-1]+)'
+    """extract diagnostic report from input text
+    input data (str): plain text read from txt file
+    return: list of str"""
+    num_re = r"([0-1]+)"
     input_numbers = re.findall(num_re, input_data)
     return input_numbers
 
-def func(numbers):
-    pre_gamma = []
-    pre_eps = []
-    for n in range(len(numbers[1])):
-        pre_gamma.append(0)
-        pre_eps.append(0)
-    for num in numbers:
-        for bul in range(len(num)):
-            if num[bul] == "1":
-                pre_gamma[bul] +=1
-                pre_eps[bul] -= 1
-            else:
-                pre_gamma[bul] -=1
-                pre_eps[bul] += 1
-    for bul in range(len(num)):
-        if pre_gamma[bul] >= 0:
-            pre_gamma[bul] = "1"
-        else:
-            pre_gamma[bul] = "0"
-        if pre_eps[bul] > 0:
-            pre_eps[bul] = "1"
-        else:
-            pre_eps[bul] = "0"
-    gamma = int("".join(pre_gamma), 2)
-    eps = int("".join(pre_eps), 2)
-    return pre_gamma, pre_eps, gamma * eps
 
-def func2(numbers, criteria):
+def rate(report, rate_type):
+    """generate gamma or epsilon rate
+    report (list of str): diagnostic report
+    rate_type (str): gamma or epsilon
+    return: binary result in form of str"""
+    pre_rate = []
+    if rate_type == "gamma":
+        rt = 1
+    elif rate_type == "epsilon":
+        rt = -1
+    else:
+        raise ValueError
+    for n in range(len(report[0])):
+        pre_rate.append(0)
+    for r in report:
+        for bul in range(len(r)):
+            if r[bul] == "1":
+                pre_rate[bul] += rt
+            elif r[bul] == "0":
+                pre_rate[bul] -= rt
+    for bul in range(len(pre_rate)):
+        if pre_rate[bul] > 0:
+            pre_rate[bul] = "1"
+        elif pre_rate[bul] < 0:
+            pre_rate[bul] = "0"
+        else:
+            if rate_type == "gamma":
+                pre_rate[bul] = "1"
+            elif rate_type == "epsilon":
+                pre_rate[bul] = "0"
+            else:
+                raise ValueError
+    rate = "".join(pre_rate)
+    return rate
+
+
+def get_power_consumption(report):
+    """generate power consumption by multiplying gamma rate and epsilon rate
+    report (list of str): diagnostic report"""
+    gamma_rate = int(rate(report, "gamma"), 2)
+    epsilon_rate = int(rate(report, "epsilon"), 2)
+    return gamma_rate * epsilon_rate
+
+
+def rate_with_bit_criteria(report, criteria):
+    """generate oxygen generator or CO2 scrubber rate
+    report (list of str): diagnostic report
+    criteria (str): oxygen or scrubber
+    return: binary result in form of str"""
     if criteria == "oxygen":
-        a = 0
+        rate_type = "gamma"
     elif criteria == "scrubber":
-        a = 1
-    for i in range(len(numbers[0])):
+        rate_type = "epsilon"
+    for i in range(len(report[0])):
         pre_res = []
-        if len(numbers) > 1:
-            example = func(numbers)[a]
-            for num in numbers:
+        if len(report) > 1:
+            example = rate(report, rate_type)
+            for num in report:
                 if num[i] == example[i]:
                     pre_res.append(num)
-            numbers = pre_res
-        else:
-            return numbers
-    return numbers
+            report = pre_res
+    return report[0]
 
+
+def get_life_support_rating(report):
+    """generate life support rating by multiplying oxygen generator rate and CO2 scrubber rate
+    report (list of str): diagnostic report"""
+    oxygen_generator_rating = int(rate_with_bit_criteria(report, "oxygen"), 2)
+    co2_scrubber_rating = int(rate_with_bit_criteria(report, "scrubber"), 2)
+    return oxygen_generator_rating * co2_scrubber_rating
 
 
 if __name__ == "__main__":
-    with open('day3_task.txt','r') as file:
+    with open("day3_ex.txt", "r") as file:
         data = file.read()
-    numbers = get_input_numbers(data)
-    print(f"The answer for the 1st task is: {func(numbers)[2]}")
-    a = int(func2(numbers, "oxygen")[0],2)
-    b = int(func2(numbers, "scrubber")[0],2)
-    print(f"The answer for the 2nd task is: {a * b}")
+    report = get_input_numbers(data)
+    print(f"The answer for the 1st task is: {get_power_consumption(report)}")
+    print(f"The answer for the 2nd task is: {get_life_support_rating(report)}")
