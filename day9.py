4916f70d9fe558f79860f14a14c4f8f4065f7731
anna-parfenova <anna.parfenova@deliveryhero.com>
2021-12-10 22:48:02 +0100

    day-9 kinda finalized

--- day9.py
+++ day9.py
@@ -1,93 +1,91 @@
-"""This programm calculates the answer
-for day 9 challenges of Advent of Code 2021"""
+"""This program calculates the answer
+for day-9 challenges of Advent-of-Code-2021"""
 
 from functools import reduce
 
+
 def get_input_numbers(input_data):
-    """extract diagnostic report from input text
-    input data (str): plain text read from txt file
-    return: list of str"""
-    input_numbers = input_data.split("\n")
-    for l in range(len(input_numbers)):
-        if input_numbers[l] == "":
-            input_numbers.remove(input_numbers[l])
+    """convert text input into matrix (list of lists(int)
+    with additional lines of max values(=9) around"""
+    heightmap = input_data.split("\n")
+    for l in range(len(heightmap)):
+        if heightmap[l] == "":
+            heightmap.remove(heightmap[l])
         else:
-            input_numbers[l] = "9" + input_numbers[l] + "9"
-            #input_numbers[l] = list(input_numbers[l])
-    input_numbers.insert(0, "9"*len(input_numbers[0]))
-    input_numbers.append("9" * len(input_numbers[0]))
-    for l in range(len(input_numbers)):
-        input_numbers[l] = list(input_numbers[l])
-        for m in range(len(input_numbers[l])):
-            input_numbers[l][m] = int(input_numbers[l][m])
-
-    return input_numbers
+            heightmap[l] = "9" + heightmap[l] + "9"
+    heightmap.insert(0, "9" * len(heightmap[0]))
+    heightmap.append("9" * len(heightmap[0]))
+    for l in range(len(heightmap)):
+        heightmap[l] = list(heightmap[l])
+        for m in range(len(heightmap[l])):
+            heightmap[l][m] = int(heightmap[l][m])
+    return heightmap
 
 
-def get_central_min(mtrx, l, m):
-    """generate ..."""
-    li1 = (mtrx[l+1][m], mtrx[l][m-1], mtrx[l][m], mtrx[l][m+1], mtrx[l-1][m])
-    li2 = (mtrx[l + 1][m], mtrx[l][m - 1], mtrx[l][m + 1], mtrx[l - 1][m])
-    if mtrx[l][m] == min(li1) and min(li1) < min(li2):
-        return mtrx[l][m] + 1
-    return 0
+def recognize_central_min(heightmap, l, m):
+    """recognize central minimum"""
+    neighbours = [
+        heightmap[l + 1][m],
+        heightmap[l][m - 1],
+        heightmap[l][m + 1],
+        heightmap[l - 1][m],
+    ]
+    cntr = heightmap[l][m]
+    if cntr == min(neighbours + [cntr]) and cntr < min(neighbours):
+        return (l, m)
+    return None
 
 
-def get_risks_sum(mtrx):
-    """generate ..."""
-    s = 0
-    for l in range(1, len(mtrx)-1):
-        for m in range(1, len(mtrx[l])-1):
-            s += get_central_min(mtrx, l, m)
-    return s
+def get_central_mins(heightmap):
+    """generates list of central minimums"""
+    central_mins = []
+    for l in range(1, len(heightmap) - 1):
+        for m in range(1, len(heightmap[l]) - 1):
+            central_min = recognize_central_min(heightmap, l, m)
+            if central_min:
+                central_mins.append(central_min)
+    return central_mins
 
 
-def flood_fill(mtrx): # THIS CALLED FLOOD-FILL ALGORITHM
-    """generate ..."""
+def get_risks_sum(heightmap):
+    """calculate sum of the risk levels of all low points on a given heightmap"""
+    central_mins = get_central_mins(heightmap)
+    risks_sum = 0
+    for central_min in central_mins:
+        risks_sum += heightmap[central_min[0]][central_min[1]] + 1
+    return risks_sum
+
+
+def flood_fill(heightmap):
+    """generate list of enclosed areas sizes (using so called FLOOD-FILL algorithm)"""
     areas = []
-    def fill(mtrx, l, m):  # , area):
+
+    def fill(heightmap, l, m):
         nonlocal area
-        if mtrx[l][m] not in [9, -1]:
-            mtrx[l][m] = -1
+        if heightmap[l][m] not in [9, -1]:
+            heightmap[l][m] = -1
             area += 1
-            # print(f"repainted: mtrx[{l}][{m}] AREA IS {area}")
-        # elif mtrx[l][m] == 9:
-        #     return area
-        # li = [mtrx[l + 1][m], mtrx[l][m - 1], mtrx[l][m + 1], mtrx[l - 1][m]]
-        if mtrx[l + 1][m] not in [9, -1]:
-            fill(mtrx, l + 1, m)  # , area)
-        if mtrx[l][m - 1] not in [9, -1]:
-            fill(mtrx, l, m - 1)  # , area)
-        if mtrx[l][m + 1] not in [9, -1]:
-            fill(mtrx, l, m + 1)  # , area)
-        if mtrx[l - 1][m] not in [9, -1]:
-            fill(mtrx, l - 1, m)  # , area)
+        for x, y in [(l + 1, m), (l, m - 1), (l, m + 1), (l - 1, m)]:
+            if heightmap[x][y] not in [9, -1]:
+                fill(heightmap, x, y)
         return
-    for l in range(1, len(mtrx)-1):
-        for m in range(1, len(mtrx[l])-1):
-            area = 0
-            fill(mtrx, l, m)
-            areas.append(area)
+
+    central_mins = get_central_mins(heightmap)
+    for l, m in central_mins:
+        area = 0
+        fill(heightmap, l, m)
+        areas.append(area)
+
     return areas
-
-
-
 
 
 if __name__ == "__main__":
     with open("day9_task.txt", "r") as file:
         data = file.read()
-    report = get_input_numbers(data)
-    # print(report)
-    print("------")
-    print(get_risks_sum(report))
-    mtrx = report.copy()
-    x = flood_fill(mtrx)
-    print("------")
-    # for m in mtrx:
-    #     print(m)
-    print("------")
+    heightmap = get_input_numbers(data)
+    print(f"The answer for the 1st task is: {get_risks_sum(heightmap)}")
+    x = flood_fill(heightmap)
     x.sort()
-    print("areas : ", reduce(lambda x, y: x*y, (x[-3:])))
-    # print(f"The answer for the 1st task is: {get_power_consumption(report)}")
-    # print(f"The answer for the 2nd task is: {get_life_support_rating(report)}")
+    print(
+        f"The answer for the 2nd task is: {reduce(lambda x, y: x*y, (x[-3:]))}"
+    )
